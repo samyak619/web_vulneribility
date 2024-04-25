86858e13d553458804e727f554bfd3ac4c72c035
samyak619 <samyakhadse@gmail.com>
2024-04-26 04:41:02 +0530

    Fix target URL in vul_scanner.py

--- vul_scanner.py
+++ vul_scanner.py
@@ -2,7 +2,7 @@
 
 import scanner as scanner
 
-target_url = "https://www.flipkart.com/"
+target_url = ""
 links_to_ignore = [""]
 # If initial scan scans a logout url, you'd want to include it in here so that you dont lose the post session
 
@@ -10,7 +10,7 @@ data_dict = {"username": "admin", "password": "password", "Login": "submit"}
 # this is for if loggin in will get you more links and you know the login info
 
 vulnerabilities_scanner = scanner.Scanner(target_url, links_to_ignore)
-vulnerabilities_scanner.session.post("https://www.flipkart.com/", data=data_dict) # this is the link to which the scanner will login and store as post
+vulnerabilities_scanner.session.post("", data=data_dict) # this is the link to which the scanner will login and store as post
 
 vulnerabilities_scanner.crawl()
 vulnerabilities_scanner.run_scanner()
